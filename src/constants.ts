df8357f273295a6dbbb6b6175e431336c54d98d4
Olivier Petiot <opetiot@gmail.com>
2024-12-24 11:16:17 +0100

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Tech People Blog';
+export const SITE_DESCRIPTION =  'Articles, stories and tutorials from Tecg People';
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
