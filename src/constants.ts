1bbda4227a0e4d7daf5394ccd9aa24d6b2b8c807
opetiot <opetiot@gmail.com>
2025-01-29 18:18:09 +0100

    Added Vercel adapter

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "Oliver's Blog";
+export const SITE_DESCRIPTION = "Viola solos and viola solos for violinists";
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLE_PER_PAGE = 6;
